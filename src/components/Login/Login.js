f9cd96c9940f9d303f1ec70f3cbab3c3d3213ebb
hyeon-bit <f.exuan21@gmail.com>
2022-05-31 11:22:46 +0900

    Login

---
+++ src/components/Login/Login.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Login.css';
+import { Button } from 'reactstrap';
+
+const Login = () => {
+  return (
+    <div className="LoginPage">
+      <div className="Box">
+        <div className="imgBox">
+          <img
+            src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+            alt="Logo"
+          />
+        </div>
+        <div className="LoginBox">
+          <input type="text" placeholder="Id" />
+          <input type="password" placeholder="Password" />
+          <Button color="primary">로그인</Button>
+        </div>
+      </div>
+      <div className="Box">
+        <p>
+          계정이 없으신가요?
+          <a href="/">가입하기</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/bootLogin/Login.js
@@ -0,0 +1,44 @@
+import './Login.css';
+import { Container, Row, Col, Form, Input, Button } from 'reactstrap';
+
+const BootstrapLogin = () => {
+    return (
+        <div className="LoginPage">
+            <Container className="bg-light border">
+                <Row style={{ rowGap: '1em', padding: '3em' }}>
+                    <Col xl={12}>
+                        <img
+                            src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+                            alt="Logo"
+                        ></img>
+                    </Col>
+                    <Col xl={12}>
+                        <Form className="LoginForm">
+                            <Input
+                                type="text"
+                                placeholder="ID"
+                                name="id"
+                            ></Input>
+                            <Input
+                                type="password"
+                                placeholder="password"
+                                name="password"
+                            ></Input>
+                            <Button type="submit" color="primary" block>
+                                로그인
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            <Container className="bg-light border">
+                <Row style={{ padding: '1em', textAlign: 'center' }}>
+                    <p>
+                        계정이 없으신가요? <a href="g">가입하기</a>
+                    </p>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+export default BootstrapLogin;
